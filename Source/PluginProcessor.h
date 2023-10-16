941352cb10ef0b3a9c7e93a5c5d56ab5b1a2035e
Lin Yudong <lqlyd@outlook.com>
2023-10-16 15:51:12 +0800

    VocalBox Init

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,8 +10,12 @@
 
 #include <JuceHeader.h>
 #include "DSP/NotchFilter.h"
+<<<<<<< Updated upstream
 #include "DSP/Saturation.h"
 #include "DSP/Convolution.h"
+=======
+#include "DSP/VocalBox.h"
+>>>>>>> Stashed changes
 
 //==============================================================================
 /**
@@ -73,8 +77,13 @@ private:
 
     double makeUpGain;
 
+<<<<<<< Updated upstream
     Saturation saturation;
 	NotchFilter notchFilter;
+=======
+	NotchFilter notchFilter;    // For testing only
+    VocalBox* vocalBox;
+>>>>>>> Stashed changes
     
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BraveLvkaiAudioProcessor)
